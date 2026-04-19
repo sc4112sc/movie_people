93e7afb68f7cdf0cafde411e16c243394cfdc7cf
sc4112sc <sc4112sc@gmail.com>
2026-04-19 14:18:46 +0800

    firebase初始化

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,15 @@ import 'package:get/get.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'theme/app_theme.dart';
 import 'pages/splash_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting('zh_TW', null);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
